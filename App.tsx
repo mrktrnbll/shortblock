a09107d744e314c0b1b042425558c622483ae48c
Mark Turnbull <mrktrnbll@gmail.com>
2025-07-25 10:47:04 +0100

    change folder location

---
+++ App.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { enableScreens } from 'react-native-screens';
+
+import HomeScreen from './src/screens/HomeScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+import LogScreen from './src/screens/LogScreen';
+
+enableScreens();
+const Tab = createBottomTabNavigator();
+
+export default function AppNavigator() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            headerShown: false,
+            tabBarStyle: {
+              backgroundColor: '#1e1e1e',
+              borderTopWidth: 0,
+              height: 70,
+              paddingBottom: 10,
+              paddingTop: 10,
+            },
+            tabBarActiveTintColor: '#4ADE80',
+            tabBarInactiveTintColor: '#94A3B8',
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                iconName = 'home-outline';
+              } else if (route.name === 'Settings') {
+                iconName = 'settings-outline';
+              } else if (route.name === 'Logs') {
+                iconName = 'list-outline';
+              }
+
+              return <Ionicons name={iconName!} size={size} color={color} />;
+            },
+          })}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Logs" component={LogScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
---
+++ metro.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const {
+  resolver: { sourceExts, assetExts },
+} = getDefaultConfig(__dirname);
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+    extraNodeModules: {
+      '@screens': path.resolve(__dirname, 'src/screens'),
+      '@components': path.resolve(__dirname, 'src/components'),
+    }
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
